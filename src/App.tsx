ebcb73dbe09d740f3d1028e9c6b29ca560cb47f9
halfurness <hal.furness@protonmail.com>
2022-12-28 22:07:08 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { VechaiProvider } from "@vechaiui/react";
 import React from "react";
 import "./App.css";
+import { VechaiProvider } from "@vechaiui/react";
 import Home from "./components/home";
 import Nav from "./components/nav";
 
-const App = () => {
+function App() {
   return (
     <VechaiProvider>
       <Nav />
       <Home />
     </VechaiProvider>
   );
-};
+}
 
 export default App;
